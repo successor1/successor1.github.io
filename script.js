2fce606055cc7e88a710db00bd172974ea5f500a
successor1 <kyildirim2002abc@gmail.com>
2025-10-04 14:50:04 +0200

    redo vids

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ You can join our telegram channel`,
 const videos = [
     {
         title: "We cling to what exists",
-        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen" allowfullscreen height="100%" src="https://streamable.com/e/rixwmx?" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
+        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen" allowfullscreen height="100%" src="https://streamable.com/e/c1khkz?" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
         date: "Unknown",
         description: "Attach. Detach."
     },
@@ -154,10 +154,22 @@ const videos = [
         description: "The collectivist spirit"
     },
     {
-        title: "Sample Video 3",
-        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen;autoplay" allowfullscreen height="100%" src="https://streamable.com/e/example3?autoplay=1" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
-        date: "January 10, 2024",
-        description: "Third sample video in the collection."
+        title: "Wake up, eat, sleep",
+        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen" allowfullscreen height="100%" src="https://streamable.com/e/h6gdvg?" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
+        date: "Unknown",
+        description: "Third video in the collection."
+    },
+    {
+        title: "Q-Day",
+        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen" allowfullscreen height="100%" src="https://streamable.com/e/5ey30l?" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
+        date: "Unknown",
+        description: "Bitcoin"
+    },
+    {
+        title: "EsCape",
+        embed: '<div style="position:relative; width:100%; height:0px; padding-bottom:56.250%"><iframe allow="fullscreen" allowfullscreen height="100%" src="https://streamable.com/e/1266qb?" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe></div>',
+        date: "Unknown",
+        description: "Number 9."
     }
 ];
 
